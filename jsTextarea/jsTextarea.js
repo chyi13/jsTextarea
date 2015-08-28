0f32032e78dbfddb4b5dc0537a2bfda0d4ed85e8
Yi Chai <yi.chai@ericsson.com>
2015-08-28 16:10:34 +0200

    Updated functions that user can type in enter.

--- jsTextarea/jsTextarea.js
+++ jsTextarea/jsTextarea.js
@@ -3,8 +3,12 @@ var ta;
 var p;
 var t;
 
+var doc;
 //
 var prevalue = "";
+
+//
+var linenumber = -1;
 
 function jsTextarea(parent, textarea) {
 	// new textarea
@@ -23,33 +27,79 @@ jsTextarea.prototype = {
 		p.insertBefore(ta, t.parentNode);
 
 
-		t.addEventListener("keydown", this.update.bind(this));
+		t.addEventListener("keyup", this.update.bind(this));
 		t.focus();
 	},
 
 	update: function() {
-		var text = t.value;
+		console.log(t.selectionStart);
+		this.poll();
+	},
 
+	poll: function() {
+		var text = t.value;
 		// Find the part of the input that is actually new
 		var same = 0,
 			l = Math.min(prevalue.length, text.length);
 		while (same < l && prevalue.charCodeAt(same) == text.charCodeAt(same)) ++same;
 		//
-		this.applyTextInput(text.slice(same), text.slice(text.length - same));
-		
+		this.applyTextInput(text.slice(same), prevalue);
+
 		prevalue = text;
+
+		setTimeout(this.poll.bind(this), 20);
 	},
 
-	applyTextInput: function(prestr, newstr) {
-		var newlines = newstr.split("\n");
-		console.log(newlines);
+	applyTextInput: function(inserted, prestr) {
+		var newlines = this.splitLines(inserted);
+		
+		// delete 
+		this.removeFromLineNumber(this.getPrevLineNumber());
+		
 		var index = 0;
-		for (index = 0; index < newlines.length; index++) {
-			ta.appendChild(this.createTextNode(newlines[index]));
+		if (inserted != "") {
+			for (index = 0; index < newlines.length; index++) {
+				ta.appendChild(this.createTextNode(newlines[index]));
+				if ((index+1) < newlines.length) {
+					ta.appendChild(this.createNewLine());
+				}
+			}
 		}
+	},
+
+	createNewLine: function() {
+		return document.createElement("br");
 	},
 
 	createTextNode: function(str) {
 		return document.createTextNode(str);
+	},
+
+	splitLines: function(str) {
+		return str.split("\n");
+	},
+	
+	removeFromLineNumber: function(n) {
+		console.log("remove ", n);
+		if (ta.getElementsByTagName("br")[n] != undefined) {
+			this.removeFromChild(ta.getElementsByTagName("br")[n]);	
+		}
+		
+	},
+	
+	removeFromChild: function(obj) {
+		while(obj.nextSibling) {
+			ta.removeChild(obj.nextSibling);
+		}
+		
+	},
+	
+	getLineNumber: function()
+	getPrevLineNumber: function() {
+		if (prevalue != "") {
+			return prevalue.split("\n").length -1;
+		}
+		else 
+			return 0;
 	}
 };
